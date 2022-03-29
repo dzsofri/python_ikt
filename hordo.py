90cb7a3a5e5ead7d562097aab3423ca988d741bc
dzsofri <beretizsofia@turr.hu>
2022-03-29 08:44:41 +0200

    5ttz5r

--- hordo.py
+++ hordo.py
@@ -6,41 +6,54 @@ import math
 def terfogat():
     r = int(mezo1.get())
     m = int(mezo2.get())
-    terfogat = round (math.pi * r * r * m )
-    liter=round(0.001*terfogat)
-    mezo3.delete(0, END)
-    mezo3.insert(0, str(liter)+' l')
-    borocska= int(mezo4.get())
     
-    telitett= round(borocska*(100/liter), 2)
-    if borocska<=liter:
+    borocska= int(mezo4.get())
+
+    if r<=0 or m<=0 or borocska<=0:
+        mezo3.delete(0, END)
+        mezo3.insert(0, 'Nem lehet nulla / negatív szám')
+        mezo5.delete(0, END)
+        mezo5.insert(0, 'Nem lehet nulla / negatív szám')
+        mezo6.delete(0, END)
+        mezo6.insert(0, 'Nem lehet nulla / negatív szám')
+
+    else:
+        terfogat = round (math.pi * r * r * m )
+        liter=round(0.001*terfogat)
         mezo3.delete(0, END)
         mezo3.insert(0, str(liter)+' l')
-        mezo5.delete(0, END)
-        mezo5.insert(0, str(liter-borocska)+' l')
-        mezo6.delete(0, END)
-        mezo6.insert(0, str(telitett)+" %")      
-    else:
-        mezo3.delete(0, END)
-        mezo3.insert(0, 'A hordó túl kicsi')
-        mezo6.delete(0, END)
-        mezo6.insert(0, 'A hordó túl kicsi')
+        telitett= round(borocska*(100/liter), 2)
+        
+        if borocska<=liter:
+            mezo3.delete(0, END)
+            mezo3.insert(0, str(liter)+' l')
+            mezo5.delete(0, END)
+            mezo5.insert(0, str(liter-borocska)+' l')
+            mezo6.delete(0, END)
+            mezo6.insert(0, str(telitett)+" %")      
+        else:
+            mezo3.delete(0, END)
+            mezo3.insert(0, 'A hordó túl kicsi')
+            mezo5.delete(0, END)
+            mezo5.insert(0, 'A hordó túl kicsi')
+            mezo6.delete(0, END)
+            mezo6.insert(0, 'A hordó túl kicsi')
 
     
 
 cimke4=Label(foablak, text="Hány liter bor (l):")
 cimke4.grid(row=1, column=1, sticky="e")
-mezo4=Entry(foablak)
+mezo4=Entry(foablak,width=30)
 mezo4.grid(row=1, column=2, columnspan=4)
 
 cimke1=Label(foablak, text="Sugár (cm):")
 cimke1.grid(row=2, column=1, sticky="e")
-mezo1=Entry(foablak)
+mezo1=Entry(foablak, width=30)
 mezo1.grid(row=2, column=2, columnspan=4)
 
 cimke2=Label(foablak, text="Magasság (cm):")
 cimke2.grid(row=3, column=1, sticky="e")
-mezo2=Entry(foablak)
+mezo2=Entry(foablak, width=30)
 mezo2.grid(row=3, column=2, columnspan=4)
 
 gomb1=Button(foablak, text="Kiszámít", command=terfogat) 
@@ -48,17 +61,17 @@ gomb1.grid(row=4, column=2, sticky="w")
 
 cimke3=Label(foablak, text="A hordó ennyi liter:")
 cimke3.grid(row=5, column=1, sticky="e")
-mezo3=Entry(foablak)
+mezo3=Entry(foablak, width=30)
 mezo3.grid(row=5, column=2, columnspan=4)
 
 cimke5=Label(foablak, text="A hordóba ennyi liter fér még:")
 cimke5.grid(row=6, column=1, sticky="e")
-mezo5=Entry(foablak)
+mezo5=Entry(foablak, width=30)
 mezo5.grid(row=6, column=2, columnspan=4)
 
 cimke6=Label(foablak, text="Telítetség")
 cimke6.grid(row=7, column=1, sticky="e")
-mezo6=Entry(foablak)
+mezo6=Entry(foablak, width=30)
 mezo6.grid(row=7, column=2, columnspan=4)
 
 foablak.mainloop()
